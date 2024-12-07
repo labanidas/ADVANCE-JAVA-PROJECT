f1cfe73c49204ef5b0807ab6e7947d5e402dc364
Swarnabha700 <chakrabortyswarnabha700@gmail.com>
2024-12-07 21:17:08 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port:3000,  
     proxy: {
       '/Medicine': 'http://localhost:9080', // Forward requests to the backend server
     },
